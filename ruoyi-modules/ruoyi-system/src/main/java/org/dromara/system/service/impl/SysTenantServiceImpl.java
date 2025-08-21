ee68904d591fd27273182870276fd184eaf9afcd
疯狂的狮子Li <15040126243@163.com>
2025-08-21 09:21:47 +0800

    fix 修复 校验租户账号余额 查询语句错误

--- ruoyi-modules/ruoyi-system/src/main/java/org/dromara/system/service/impl/SysTenantServiceImpl.java
+++ ruoyi-modules/ruoyi-system/src/main/java/org/dromara/system/service/impl/SysTenantServiceImpl.java
@@ -347,8 +347,8 @@ public class SysTenantServiceImpl implements ISysTenantService {
         if (tenant.getAccountCount() == -1) {
             return true;
         }
-        Long userNumber = userMapper.selectCount(new LambdaQueryWrapper<>());
-        // 如果余额大于0代表还有可用名额
+        Long userNumber = userMapper.selectCount(
+            new LambdaQueryWrapper<SysUser>().eq(SysUser::getTenantId, tenantId));        // 如果余额大于0代表还有可用名额
         return tenant.getAccountCount() - userNumber > 0;
     }
 
