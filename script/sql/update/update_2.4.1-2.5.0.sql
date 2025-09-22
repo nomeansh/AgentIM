789273d0d9af69b36335bbd9d66f0500bd681040
疯狂的狮子Li <15040126243@163.com>
2025-09-22 11:09:00 +0800

    Revert "update warm-flow 升级 1.8.2-m2"
    This reverts commit 0fff64da20cfa3733ee2b8e2344b1193886ff6a1.

--- script/sql/update/update_2.4.1-2.5.0.sql
+++ script/sql/update/update_2.4.1-2.5.0.sql
@@ -64,28 +64,3 @@ update sys_menu set remark = '/system/user' where menu_id = 131;
 update sys_menu set remark = '/system/dict' where menu_id = 132;
 update sys_menu set remark = '/system/oss' where menu_id = 133;
 update sys_menu set remark = '/workflow/processDefinition' where menu_id = 11700;
-
-ALTER TABLE `flow_definition`
-    ADD COLUMN `create_by` varchar(64) NULL DEFAULT NULL COMMENT '创建人' AFTER `create_time`,
-    ADD COLUMN `update_by` varchar(64) NULL DEFAULT NULL COMMENT '更新人' AFTER `update_time`;
-
-ALTER TABLE `flow_node`
-    ADD COLUMN `create_by` varchar(64) NULL DEFAULT NULL COMMENT '创建人' AFTER `create_time`,
-    ADD COLUMN `update_by` varchar(64) NULL DEFAULT NULL COMMENT '更新人' AFTER `update_time`;
-
-ALTER TABLE `flow_skip`
-    ADD COLUMN `create_by` varchar(64) NULL DEFAULT NULL COMMENT '创建人' AFTER `create_time`,
-    ADD COLUMN `update_by` varchar(64) NULL DEFAULT NULL COMMENT '更新人' AFTER `update_time`;
-
-ALTER TABLE `flow_instance`
-    ADD COLUMN `update_by` varchar(64) NULL DEFAULT NULL COMMENT '更新人' AFTER `update_time`;
-
-ALTER TABLE `flow_task`
-    ADD COLUMN `create_by` varchar(64) NULL DEFAULT NULL COMMENT '创建人' AFTER `create_time`,
-    ADD COLUMN `update_by` varchar(64) NULL DEFAULT NULL COMMENT '更新人' AFTER `update_time`;
-
-ALTER TABLE `flow_his_task`
-    ADD COLUMN `update_by` varchar(64) NULL DEFAULT NULL COMMENT '更新人' AFTER `update_time`;
-
-ALTER TABLE `flow_user`
-    ADD COLUMN `update_by` varchar(64) NULL DEFAULT NULL COMMENT '更新人' AFTER `update_time`;
